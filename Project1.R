207cc74d3dd428bf07c0c6bfc72affdbf1a639f8
cnkaufmann <chris.n.kaufmann@gmail.com>
2017-07-06 18:24:18 -0700

    Adding recent updates

--- Project1.R
+++ Project1.R
@@ -13,19 +13,32 @@ for (year in c_years) {
 }
 
 #In each exported folder, change the .do file to be linked to the correct file [Notice the regular expressions!!!]:
-for (year in c_years) {
-  dofile_act <- paste("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_", year, "/atusact_", year, ".do", sep="")
-  fileold <- readLines(dofile_act)
-  filenew <- gsub(pattern = paste("c:\\\\atusact_", year, ".dat", sep=""), replace = paste("\"/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_", year, "/atusact_", year, ".dat\"", sep=""), x=fileold)
-  filepath <- paste('save \"/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/FilesToImport/atusact_', year, '.dta\", replace', sep="")
-  filenew <- append(filenew, filepath)
-  writeLines(filenew, con=dofile_act)
-}
+#Decided to go a different approach, so will not use this section. Commenting out for now:
+#for (year in c_years) {
+#  dofile_act <- paste("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_", year, "/atusact_", year, ".do", sep="")
+#  fileold <- readLines(dofile_act)
+#  filenew <- gsub(pattern = paste("c:\\\\atusact_", year, ".dat", sep=""), replace = paste("\"/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_", year, "/atusact_", year, ".dat\"", sep=""), x=fileold)
+#  filepath <- paste('save \"/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/FilesToImport/atusact_', year, '.dta\", replace', sep="")
+#  filenew <- append(filenew, filepath)
+#  filenew <- append(filenew, "clear all")
+#  filenew <- append(filenew, "exit")
+#  writeLines(filenew, con=dofile_act)
+#}
 
-#Using R Package: RStata
-install.packages("RStata")
-options("RStata.StataPath" = "/Applications/Stata/StataSE.app/Contents/MacOS/stata-se") #Setup Path
-options("RStata.StataVersion" = 13) #Setup Version
+atuact_2003 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2003/atusact_2003.dat")
+atuact_2004 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2004/atusact_2004.dat")
+atuact_2005 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2005/atusact_2005.dat")
+atuact_2006 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2006/atusact_2006.dat")
+atuact_2007 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2007/atusact_2007.dat")
+atuact_2008 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2008/atusact_2008.dat")
+atuact_2009 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2009/atusact_2009.dat")
+atuact_2010 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2010/atusact_2010.dat")
+atuact_2011 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2011/atusact_2011.dat")
+atuact_2012 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2012/atusact_2012.dat")
+atuact_2013 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2013/atusact_2013.dat")
+atuact_2014 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2014/atusact_2014.dat")
+atuact_2015 <- read.csv("/Users/christopherkaufmann/Datasets/Time Use Survey/DataSciencePort/atusact_2015/atusact_2015.dat")
+
 
 
 
